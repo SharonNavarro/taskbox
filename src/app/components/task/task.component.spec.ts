efbdb1f51fe83a969c63f4b350a9255e33bb856e
SharonNavarro <shanmowrk@gmail.com>
2024-07-07 15:30:22 -0500

    style: task component

--- src/app/components/task/task.component.spec.ts
+++ src/app/components/task/task.component.spec.ts
@@ -1,6 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { TaskComponent } from './task.component';
+import  TaskComponent  from './task.component';
 
 describe('TaskComponent', () => {
   let component: TaskComponent;
@@ -11,7 +11,7 @@ describe('TaskComponent', () => {
       imports: [TaskComponent]
     })
     .compileComponents();
-    
+
     fixture = TestBed.createComponent(TaskComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
